adb3be4deafdc32262449a8276edb78239cfd532
megasrd <reganjduthie@gmail.com>
2025-04-12 22:54:17 +0200

    added styling basic structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import styles from "./page.module.css";
+import '../../styles/global.scss';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className='container'>
       {/* Job Listing */}
     </div>
   );

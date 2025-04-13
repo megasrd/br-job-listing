2b323e96fbe24f6ff902d776842ece348710d96e
megasrd <reganjduthie@gmail.com>
2025-04-13 19:58:11 +0200

    added ProtectedRoute component

---
+++ src/app/components/Auth/ProtectedRoute.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkUser } from "../../store/slices/userSlice";
+
+export default function ProtectedRoute({ children }) {
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const { isLoggedIn, isValid } = useSelector((state) => state.user);
+
+    //authentication check when component mounts
+    useEffect(() => {
+        if (!isLoggedIn) {
+            const isAuthenticated = dispatch(checkUser());
+            if (!isAuthenticated) {
+                router.push('/login');
+            }
+        }
+    }, [dispatch, isLoggedIn, router])
+
+    // If not logged in, do not render the children
+    if (!isLoggedIn) {
+        return null;
+    }
+    return children;
+}

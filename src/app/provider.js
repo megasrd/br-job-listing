46c65f21b032f04ff9c15bb82868eff14047f0dd
megasrd <reganjduthie@gmail.com>
2025-04-13 15:53:17 +0200

    fetch data from mock api then set it to jobs store

--- src/app/provider.js
+++ src/app/provider.js
@@ -1,10 +1,27 @@
 'use client';
 
+import { useEffect } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import store from './store';
-import { Provider } from 'react-redux';
+import { fetchJobsStart, fetchJobsSuccess, fetchJobsFailure } from "./store/slices/jobsSlice"
 
 function Initializer({ children }) {
-    console.log("Initializing Redux store");
+
+    useEffect(() => {
+        // Initialize any global state or settings here
+        console.log("Redux Provider Initialized");
+        //Fetch jobs data then set it.
+        store.dispatch({ type: 'jobs/fetchJobsStart'});
+        
+        fetch('http://localhost:3001/jobs')
+            .then(response => response.json())
+            .then(data => {
+                store.dispatch({ type: 'jobs/fetchJobsSuccess', payload: data })
+            })
+            .catch(error => {
+                store.dispatch({ type: 'jobs/fetchJobsFailure', payload: error.message });
+            });
+    }, []);
     return <>{children}</>;
 }
 

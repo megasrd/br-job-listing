f2e296fe9eb46af1858b9aa0a0993e0adac57aaf
megasrd <reganjduthie@gmail.com>
2025-04-13 14:51:05 +0200

    configure layout.js to include the ReduxProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
 import '../../styles/global.scss';
 
+import { ReduxProvider } from './provider.js';
 import NavigationBar from "./components/NavigationBar/NavigationBar.js";
 
 export default function Layout({ children }) {
   return (
     <html lang="en">      
       <body>
-        <NavigationBar />
-        <div className="container page-wrapper">
-          {children}
-        </div>
+        <ReduxProvider>
+          <>
+            <NavigationBar />
+            <div className="container page-wrapper">
+              {children}
+            </div>          
+          </>
+        </ReduxProvider>
       </body>
     </html>
   );

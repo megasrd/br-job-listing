b77ab09e2b5320d1aecb41e15667b14cc9f705b8
megasrd <reganjduthie@gmail.com>
2025-04-14 10:33:49 +0200

    fixed NaviationBar to use Link component for routing

--- src/app/components/NavigationBar/NavigationBar.js
+++ src/app/components/NavigationBar/NavigationBar.js
@@ -2,6 +2,7 @@
 
 import './NavigationBar.scss';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { useRouter, usePathname } from 'next/navigation';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../store/slices/userSlice';
@@ -26,15 +27,15 @@ const NavigationBar = () => {
     return (
         <nav className="navigation-bar">
             <ul className="container navigation-bar__wrapper">
-                <a href="/" className={`navigation-bar__item ${pathname == '/' ? 'active' : ''}`}> Find Jobs </a>
-                <a href="/new-job" className="navigation-bar__item"> Post A New Job </a>
+                <Link href="/" className={`navigation-bar__item ${pathname == '/' ? 'active' : ''}`}> Find Jobs </Link>
+                <Link href="/new-job" className={`navigation-bar__item ${pathname == '/new-job' ? 'active' : ''}`}> Post A New Job </Link>
                 { isLoggedIn && <div className="navigation-bar__user-details"> 
                     <img className="navigation-bar__user-details--avatar" width="32" src={avatar} />
-                    <label className="navigation-bar__user-details--username"> { username } </label>
+                    <label title={email} className="navigation-bar__user-details--username"> { username } </label>
                     <a onClick={ () => { handleLogout() } } className="navigation-bar__item navigation-bar__logout"> Logout </a>
                 </div> }
                 { !isLoggedIn && <div className="navigation-bar__user-details">
-                    <a href="/login" className={`navigation-bar__item ${pathname == '/login' ? 'active' : ''}`}> Login </a>
+                    <Link href="/login" className={`navigation-bar__item ${pathname == '/login' ? 'active' : ''}`}> Login </Link>
                 </div>}
             </ul>
         </nav>

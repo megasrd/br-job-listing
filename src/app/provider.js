8af0b7b58d21439963b671d936e588c7c51069a4
megasrd <reganjduthie@gmail.com>
2025-04-15 22:19:23 +0200

    Refactor Initializer to use axios instead of fetch

--- src/app/provider.js
+++ src/app/provider.js
@@ -3,22 +3,26 @@
 import { useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
 import { checkUser } from "../app/store/slices/userSlice";
+import axios from 'axios';
 
 function Initializer({ children, store }) {
     useEffect(() => {
         console.log("Fetching jobs data...");
-        //Fetch jobs data then set it.
-        //dispatch intial action
         store.dispatch({ type: 'jobs/fetchJobsStart'});
-        fetch('https://job-mongo-db-a076e6ade381.herokuapp.com/jobs')
-            .then(response => response.json())
-            .then(data => {
-                store.dispatch({ type: 'jobs/fetchJobsSuccess', payload: data })
-            })
-            .catch(error => {
-                store.dispatch({ type: 'jobs/fetchJobsFailure', payload: error.message });
-            });
+        
+        axios.get('https://job-mongo-db-a076e6ade381.herokuapp.com/jobs', {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        .then(response => {
+            store.dispatch({ type: 'jobs/fetchJobsSuccess', payload: response.data })
+        })
+        .catch(error => {
+            store.dispatch({ type: 'jobs/fetchJobsFailure', payload: error.message });
+        });
     }, []);
+    
     return (
         <>
             {children}
